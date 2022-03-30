efc145613a296f49438a7efeb4ff7f70f81f73bf
JJJJisun <42526264+JJJJisun@users.noreply.github.com>
2022-03-30 23:46:07 +0900

    compose_1

--- app/src/main/java/com/jjjjisun/firstcompose/MainActivity.kt
+++ app/src/main/java/com/jjjjisun/firstcompose/MainActivity.kt
@@ -1,109 +1,116 @@
 package com.jjjjisun.firstcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jjjjisun.firstcompose.ui.theme.FirstComposeTheme
-import kotlin.math.log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            // 프로젝트이름
-            FirstComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("안드입니다?!")
-                }
+            FirstComposeTheme() {
+                Conversation(SampleData.conversationSample)
             }
         }
     }
 }
 
-// 뷰 -> 매개변수를 받아서 텍스트를 생성
-// Text 자체에 모디파이어나 스타일을 지정 가능
 @Composable
-fun Greeting(name: String) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("안드로이드 첫 컴포즈") },
-                backgroundColor = Color(0xfff25287)
-            )
-        },
-        floatingActionButtonPosition = FabPosition.End,
-        floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
-                Text("클릭")
-            }
-        },
-    )
-    {
-//        Text(text = "안녕하세요?! $name!")
-        MyComposableView()
-    }
-}
-
-@Composable
-fun MyComposableView() {
-    Log.d("tag", "MyComposableView: 호출")
-
-    // 리니어 레이아웃 - 버티컬
-    Column(
-        Modifier
-            .background(Color.Green)
-            .verticalScroll(rememberScrollState())
-    ) {
-        for (i in 0..30) {
-            MyRowItem()
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
-@Composable
-fun MyRowItem() {
+data class Message(val author: String, val body: String)
 
-    // 리니어 레이아웃 - 호리젠탈
-    Row(
-        Modifier
-            .padding(10.dp)
-            .background(Color(0xffeaffd0))
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "안녕하세요!",
-            Modifier
-                .padding(all = 10.dp)
-                .background(Color.Yellow)
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.git),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
-        Spacer(modifier = Modifier.size(10.dp))
-        Text(text = "안녕하세요!")
-        Spacer(modifier = Modifier.size(10.dp))
-        Text(text = "안녕하세요!")
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // 메세지가 확장되는지의 여부를 추적
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor은 한 색상에서 다른 색상으로 바뀜
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) {
+                MaterialTheme.colors.primary
+            } else {
+                MaterialTheme.colors.surface
+            }
+                )
+
+        // 이 열을 클릭하면 isExpanded 변수가 전환
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+
+                // 클릭 시 색상 변경 primary -> surface
+                color = surfaceColor,
+
+                // 클릭 시 사이즈 변경
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(4.dp),
+
+                    // 만약 메세지가 클릭 시 확장되면 모든 내용이 표시
+                    // 그렇지 않으면 첫번째 줄만 표시
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
     }
 }
 
-// 미리보기
-// 하지만 setContent 뷰와는 별개
+// 프리뷰
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewConversation() {
     FirstComposeTheme {
-        Greeting("Android")
+        Conversation(SampleData.conversationSample)
     }
 }
